a784b4fd8cc5d192b9b2058ad774611894a3417d
Jan Kolomazník <jan.kolomaznik@mendelu.cz>
2016-02-24 13:53:21 +0100

    Automatické resize obrázku podle okna + zavedení toolbaru.

--- src/cz/vianel/drawing/Main.java
+++ src/cz/vianel/drawing/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -19,7 +20,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("screen.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Drawing");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
 
--- src/cz/vianel/drawing/ScreenController.java
+++ src/cz/vianel/drawing/ScreenController.java
@@ -3,21 +3,34 @@ package cz.vianel.drawing;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ScreenController {
+public class ScreenController  implements Initializable {
 
     @FXML
     private ImageView imageView;
+
+    @FXML
+    private BorderPane borderPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        imageView.fitWidthProperty().bind(borderPane.widthProperty());
+        imageView.fitHeightProperty().bind(borderPane.heightProperty());
+    }
 
     public void openImage(ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
